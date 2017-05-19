7d0f133de4a6421d183f9877edc61297b5c5a1fa
huqianqian <huqianqian_giser@126.com>
2017-05-19 16:08:18 +0800

    jtsdemo initial version

---
+++ src/main/java/com/sogou/go2map/App.java
@@ -0,0 +1,13 @@
+package com.sogou.go2map;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
